a96e299d3b2b8c316a511cef50f92471f243b7dc
abkrym5555 <momo542004@gmail.com>
2026-04-15 01:54:40 +0200

    make file & folder structure dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import FileRecComp from "./components/FileRecComp";
+import { fileTree } from "./data";
+
 function App() {
-  return <div className="m-7"></div>;
+  return (
+    <div className="m-7">
+      <FileRecComp filetree={fileTree} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/FileRecComp.tsx
+++ src/components/FileRecComp.tsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
+import type { IFile } from "../interfaces";
 import FileIcon from "./SVG/FileIcon";
+import FolderIcon from "./SVG/FolderIcon";
 import RightArrowIcon from "./SVG/RightArrowIcon";
+import BottomIcon from "./SVG/BottomIcon";
 
 interface IFileComponentProps {
-  name: string;
+  filetree: IFile;
 }
 
-function FileRecComp({ name }: IFileComponentProps) {
+function FileRecComp({ filetree }: IFileComponentProps) {
+  const [isopen, setIsopen] = useState(true);
   return (
-    <div className="flex gap-2 items-center">
-      <RightArrowIcon />
-      <FileIcon />
-      <span>{name}</span>
+    <div className="cursor-pointer ml-2">
+      <div className="flex gap-1 items-center mb-2">
+        {filetree.isFolder ? (
+          <div className="flex gap-2" onClick={() => setIsopen((pr) => !pr)}>
+            {isopen ? <BottomIcon /> : <RightArrowIcon />}
+            <FolderIcon />
+            <span>{filetree.name}</span>
+          </div>
+        ) : (
+          <>
+            <span className="ml-5"></span>
+            <FileIcon />
+            <span>{filetree.name}</span>
+          </>
+        )}
+      </div>
+
+      {isopen
+        ? filetree.children &&
+          filetree.children.map((file) => (
+            <FileRecComp key={filetree.id} filetree={file} />
+          ))
+        : null}
     </div>
   );
 }
